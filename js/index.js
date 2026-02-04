a966deb47908a88ab11063c4fbb0a459f5a77fe7
RKBoss6 <kabironkar@gmail.com>
2026-02-04 12:41:27 -0500

    Refactor filter label update logic

--- js/index.js
+++ js/index.js
@@ -733,21 +733,21 @@ function refreshLibrary(options) {
     searchValue = "";
     searchChip = searchValue;
   }
-let filtersContainer = document.querySelector("#librarycontainer .filter-nav");
-let filterToggle = filtersContainer.querySelector('.dropdown-toggle span');
-let filterAnchors = filtersContainer.querySelectorAll('.menu-item a');
-
-// Find the currently selected filter and update label
-let activeFilterAnchor = Array.from(filterAnchors).find(a => 
-  a.getAttribute('dt') === (searchChip || '')
-);
-
-if (activeFilterAnchor && filterToggle) {
-  filterToggle.innerHTML = '';
-  filterToggle.innerHTML += '<svg class="inline-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6"><path fill-rule="evenodd" d="M3.792 2.938A49.069 49.069 0 0 1 12 2.25c2.797 0 5.54.236 8.209.688a1.857 1.857 0 0 1 1.541 1.836v1.044a3 3 0 0 1-.879 2.121l-6.182 6.182a1.5 1.5 0 0 0-.439 1.061v2.927a3 3 0 0 1-1.658 2.684l-1.757.878A.75.75 0 0 1 9.75 21v-5.818a1.5 1.5 0 0 0-.44-1.06L3.13 7.938a3 3 0 0 1-.879-2.121V4.774c0-.897.64-1.683 1.542-1.836Z" clip-rule="evenodd" /></svg>';
-  filterToggle.innerHTML += activeFilterAnchor.textContent;
-
-}
+  let filtersContainer = document.querySelector("#librarycontainer .filter-nav");
+  let filterToggle = filtersContainer.querySelector('.dropdown-toggle span');
+  let filterAnchors = filtersContainer.querySelectorAll('.menu-item a');
+  
+  // Find the currently selected filter and update label
+  let activeFilterAnchor = Array.from(filterAnchors).find(a => 
+    a.getAttribute('dt') === (searchChip || '')
+  );
+  
+  if (activeFilterAnchor && filterToggle) {
+    filterToggle.innerHTML = '';
+    filterToggle.innerHTML += '<svg class="inline-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6"><path fill-rule="evenodd" d="M3.792 2.938A49.069 49.069 0 0 1 12 2.25c2.797 0 5.54.236 8.209.688a1.857 1.857 0 0 1 1.541 1.836v1.044a3 3 0 0 1-.879 2.121l-6.182 6.182a1.5 1.5 0 0 0-.439 1.061v2.927a3 3 0 0 1-1.658 2.684l-1.757.878A.75.75 0 0 1 9.75 21v-5.818a1.5 1.5 0 0 0-.44-1.06L3.13 7.938a3 3 0 0 1-.879-2.121V4.774c0-.897.64-1.683 1.542-1.836Z" clip-rule="evenodd" /></svg>';
+    filterToggle.innerHTML += activeFilterAnchor.textContent;
+  
+  }
   // update the search box value
   if (!options.dontChangeSearchBox) {
     if (searchType === "full")
